bbc1d42fb425f18db47dbf5208edb8182d62f859
RRodrigues <rrodriguesicc@gmail.com>
2017-12-05 02:14:25 -0300

    Recebeendo argumentos da MainActivity

--- app/src/main/java/com/projeto/clubedowhisky/tabs/MyTicketsFragment.java
+++ app/src/main/java/com/projeto/clubedowhisky/tabs/MyTicketsFragment.java
@@ -7,9 +7,11 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.projeto.clubedowhisky.R;
 import com.projeto.clubedowhisky.TicketActivity;
+import com.projeto.clubedowhisky.classes.Clients;
 
 /**
  * Created by pedrolima on 30/11/2017.
@@ -21,6 +23,11 @@ public class MyTicketsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_mytickets_main, container, false);
+
+        Clients c = (Clients) getArguments().getSerializable("client");
+
+        Toast toast = Toast.makeText(getContext(), c.getUser().getName(), Toast.LENGTH_LONG);
+        toast.show();
 
         mFab = root.findViewById(R.id.fab);
         mFab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/projeto/clubedowhisky/tabs/QrcodeFragment.java
+++ app/src/main/java/com/projeto/clubedowhisky/tabs/QrcodeFragment.java
@@ -6,7 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.zxing.Result;
 import com.projeto.clubedowhisky.R;
+
+import org.xmlpull.v1.XmlPullParser;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -14,18 +17,31 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
  * Created by pedrolima on 30/11/2017.
  */
 
-public class QrcodeFragment extends Fragment{
+public class QrcodeFragment extends Fragment implements ZXingScannerView.ResultHandler{
 
-    ZXingScannerView scanner;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
+    ZXingScannerView mScannerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_qrcode, container, false);
+        mScannerView = new ZXingScannerView(getActivity());
+        return mScannerView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+
     }
 }
