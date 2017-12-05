c0e128c67328b0f05b58d8a9af1063e8387b1a04
RRodrigues <rrodriguesicc@gmail.com>
2017-12-05 02:05:25 -0300

    criando task de salvar na api; criando asynctaskloader para inserir client

--- app/src/main/java/com/projeto/clubedowhisky/httpClient/ClientParser.java
+++ app/src/main/java/com/projeto/clubedowhisky/httpClient/ClientParser.java
@@ -1,5 +1,6 @@
 package com.projeto.clubedowhisky.httpClient;
 
+import com.google.gson.Gson;
 import com.projeto.clubedowhisky.classes.Clients;
 import com.projeto.clubedowhisky.services.ClientApiService;
 
@@ -7,6 +8,8 @@ import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -23,7 +26,7 @@ public class ClientParser {
                 .build();
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://localhost:8080/clubedowhisky-API/")
+                .baseUrl("http://192.168.43.96:8080/clubedowhisky-API/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build();
@@ -32,7 +35,29 @@ public class ClientParser {
 
         Clients clients = api.logar(username, password).execute().body();
 
-        if (client != null) {
+        if (clients != null) {
+            return clients;
+        }
+        return null;
+    }
+
+    public static Clients save(String username, String password, String email, String name) throws IOException {
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.43.96:8080/clubedowhisky-API/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        ClientApiService api = retrofit.create(ClientApiService.class);
+
+        Clients clients = api.save(username, password, email, name).execute().body();
+
+        if (clients != null) {
             return clients;
         }
         return null;
--- app/src/main/java/com/projeto/clubedowhisky/services/ClientApiService.java
+++ app/src/main/java/com/projeto/clubedowhisky/services/ClientApiService.java
@@ -3,7 +3,9 @@ package com.projeto.clubedowhisky.services;
 import com.projeto.clubedowhisky.classes.Clients;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 /**
@@ -14,4 +16,7 @@ public interface ClientApiService {
 
     @GET("client/logar")
     Call<Clients> logar(@Query("username") String username, @Query("password") String password);
+
+    @GET("client/save")
+    Call<Clients> save(@Query("username") String username, @Query("password") String password, @Query("email") String email, @Query("name") String name);
 }
